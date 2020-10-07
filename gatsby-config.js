b4a965ce401b9080e110617cbff59d600aedda3d
ingolf <ingolf.mail@yandex.ru>
2020-10-07 15:28:44 +0300

    Remove unused packages

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,22 +1,7 @@
 module.exports = {
   plugins: [
     `gatsby-plugin-react-helmet`,
-    `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
     `gatsby-plugin-sass`,
     `gatsby-plugin-sitemap`,
-    {
-      resolve: `gatsby-plugin-google-analytics`,
-      options: {
-        trackingId: "YOUR_GOOGLE_ANALYTICS_TRACKING_ID",
-      },
-    },
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        name: `images`,
-        path: `${__dirname}/src/images`,
-      },
-    },
   ],
 }
