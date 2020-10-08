5f7e174817b39957ad51a068bd7e1f59f1f50198
ingolf <ingolf.mail@yandex.ru>
2020-10-08 03:15:40 +0300

    Team section

---
+++ src/components/team-gallery/team-gallery.jsx
@@ -0,0 +1,38 @@
+import React, { useMemo } from 'react';
+import { isMobile } from 'react-device-detect';
+
+import './team-gallery.scss';
+
+const TeamGallery = ({items = []}) => {
+    const gallery = useMemo(() => {
+        return items.map(({img, name, duty}) => {
+            const rotate = isMobile
+                ? 0
+                : 25 * Math.random() - 15;
+
+            return (
+                <div
+                    className="item"
+                    style={{
+                        '--rotate': `${rotate}deg`,
+                        '--name': `"${name} ${duty}"`,
+                    }}
+                >
+                    <img
+                        className="item-img"
+                        src={img}
+                        alt={name}
+                    />
+                </div>
+            );
+        });
+    }, [items]);
+
+    return (
+        <div className="team-gallery">
+            {gallery}
+        </div>
+    );
+};
+
+export default TeamGallery;
