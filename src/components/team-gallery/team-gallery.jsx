03d0e5144a28d3da0f303d7206df4e694b72bc93
ingolf <ingolf.mail@yandex.ru>
2020-10-08 03:50:39 +0300

    Fix gallery

--- src/components/team-gallery/team-gallery.jsx
+++ src/components/team-gallery/team-gallery.jsx
@@ -4,29 +4,27 @@ import { isMobile } from 'react-device-detect';
 import './team-gallery.scss';
 
 const TeamGallery = ({items = []}) => {
-    const gallery = useMemo(() => {
-        return items.map(({img, name, duty}) => {
-            const rotate = isMobile
-                ? 0
-                : 25 * Math.random() - 15;
+    const gallery = items.map(({img, name, duty}) => {
+        const rotate = isMobile
+            ? 0
+            : 25 * Math.random() - 15;
 
-            return (
-                <div
-                    className="item"
-                    style={{
-                        '--rotate': `${rotate}deg`,
-                        '--name': `"${name} ${duty}"`,
-                    }}
-                >
-                    <img
-                        className="item-img"
-                        src={img}
-                        alt={name}
-                    />
-                </div>
-            );
-        });
-    }, [items]);
+        return (
+            <div
+                className="item"
+                style={{
+                    '--rotate': `${rotate}deg`,
+                    '--name': `"${name} ${duty}"`,
+                }}
+            >
+                <img
+                    className="item-img"
+                    src={img}
+                    alt={name}
+                />
+            </div>
+        );
+    });
 
     return (
         <div className="team-gallery">
